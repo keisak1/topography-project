7f6670fda644b4d3bc0c364d994b979d16388939
ufpnuno <77934211+ufpnuno@users.noreply.github.com>
2023-02-25 14:38:52 +0000

    Login screen by: @hosain-mohamed

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:topography_project/main.dart';
+import 'package:topography_project/src/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
